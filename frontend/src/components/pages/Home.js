6b23857d9cfdc6607fd9009e21ccf49e10242c08
RobinGerster <gersterrobyn@t-online.de>
2021-10-19 12:07:35 -0400

    Quick Addition Of A Responsive Grid

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -3,7 +3,13 @@ import Navbar from "../../components/navbar/Navbar";
 export default  function Home(){
     return (
         <div className={"Home"}>
-            <Navbar />
+            <Navbar/>
+            <div class="ui stackable four column grid">
+                <div class="column">stackable column 1</div>
+                <div class="column">stackable column 1</div>
+                <div class="column">stackable column 1</div>
+                <div class="column">stackable column 1</div>
+            </div>
       </div>
     )
 }
