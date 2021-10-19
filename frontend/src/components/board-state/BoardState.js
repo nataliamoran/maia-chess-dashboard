535c05b15ab90145a0e3772f7aff61631e25ced5
angel <angelyuan233@gmail.com>
2021-10-19 17:59:57 -0400

    add board state

---
+++ frontend/src/components/board-state/BoardState.js
@@ -0,0 +1,49 @@
+//import * as ReactBootStrap from "react-bootstrap"
+import { ListGroup, Card } from "react-bootstrap";
+//import React, { Component } from "react";
+import React from "react";
+
+class BoardState extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+          data: [{ID: "123", whitePlayer: "user1", blackPlayer: "user2", date: "04/22/20 4:58AM", state: { round: 1, move: "e4 c1", FEN: ""}},
+          {ID: "1", whitePlayer: "xxx", blackPlayer: "yyy", date: "04/09/20 9:00PM", state: { round: 6, move: "e4 c1", FEN: ""}}],
+          curr: ""
+        }
+      }
+
+      getData(){
+       //TODO
+      }
+      componentDidMount(){
+        this.getData()
+      }
+
+
+    render() {
+        return (
+            <Card bg="dark" variant="dark" style={{ width: '200px'}}>
+                <Card.Body>
+                    <Card.Title style={{color:'white'}}>Board State</Card.Title>
+                    <ListGroup variant="flush" style={{ background: 'grey' }}>
+                    {this.state.data.map(d => (
+                    
+                        <ListGroup.Item key={d.ID}
+                        action variant="dark" 
+                        onClick={() => {this.state.curr = d.ID; console.log(this.state.curr);}} >
+                            <div>{d.whitePlayer} vs {d.blackPlayer}</div>
+                            <div>{d.date}</div>
+                            <div>{d.state.round}&nbsp;{d.state.move}</div>
+                        </ListGroup.Item>
+
+                    ))} 
+                    </ListGroup>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
+
+export default BoardState
