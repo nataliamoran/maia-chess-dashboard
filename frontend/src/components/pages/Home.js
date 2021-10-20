6f9661c4ed146d946897d469348fc75a10bb0ff9
angel <angelyuan233@gmail.com>
2021-10-19 22:50:26 -0400

    add callback

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import Navbar from "../../components/navbar/Navbar";
+import BoardState from "../../components/board-state/BoardState";
 export default  function Home(){
+
+    const boardHandleCallback = (childData) =>{
+        console.log(childData);
+        //this.setState({data: childData})
+    }
+
     return (
         <div className={"Home"}>
             <Navbar />
+            <BoardState  parentCallback = {boardHandleCallback}/>
       </div>
     )
 }
+
