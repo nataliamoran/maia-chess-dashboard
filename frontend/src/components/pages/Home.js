8ba4492b3748565a74a489ce469bced47ff8dcc7
angel <angelyuan233@gmail.com>
2021-10-21 15:58:18 -0400

    changing the general ui

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -4,24 +4,37 @@ import BoardState from "../../components/board-state/BoardState";
 import FindMenu from "../../components/findMenu/FindMenu";
 
 export default  function Home(){
+    const getWindowDimensions = () => {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+          width,
+          height
+        };
+      }
 
-    const boardHandleCallback = (childData) =>{
-        console.log(childData);
+    const boardHandleCallback = (ID, FEN) =>{
+        console.log(ID);
+        console.log(FEN);
         //this.setState({data: childData})
     }
 
+    //const { height, width } = getWindowDimensions();
+
     return (
-        <div className={"Home"}>
+        <body style={{"background": "#6a6970", height: "580px"}}>
+        <div className={"Home"} >
             <Navbar/>
-            <div className="ui stackable four column grid">
-                <div className="column">
+            <div className="ui stackable four column grid" style={{marginTop: "10px"}}>
+                <div className="column" style={{width: "230px"}}>
                     <FindMenu/>
+                    
                 </div>
-                <div className="column"> <BoardState  parentCallback = {boardHandleCallback}/></div>
+                <div className="column" style={{width: "230px"}}> <BoardState  parentCallback = {boardHandleCallback}/></div>
                 <div className="column">stackable column 1</div>
                 <div className="column">stackable column 1</div>
             </div>
       </div>
+      </body>
     )
 }
 
