0efc6a49c64e189367b154d2c4608b4e785d03c7
nataliamoran <natalja.moran@gmail.com>
2021-10-23 00:39:25 -0400

    Comment db

--- backend/api/example_student_api.py
+++ backend/api/example_student_api.py
@@ -10,7 +10,8 @@ from .api_main import app
 
 import motor.motor_asyncio
 
-MONGODB_URL = os.environ.get("MONGODB_URL", 'mongodb://localhost:27017')
+# MONGODB_URL = os.environ.get("MONGODB_URL", 'mongodb://localhost:27017')
+MONGODB_URL = 'mongodb://localhost:27017'
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 db = client.college
